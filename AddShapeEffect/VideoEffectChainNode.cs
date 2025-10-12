68784983116ca64388f3407b8da75e79aa15da6b
ハイさん <saisaisaiyoshiyoshiyoshi@gmail.com>
2025-10-12 16:35:11 +0900

    いらんusingけした

--- AddShapeEffect/VideoEffectChainNode.cs
+++ AddShapeEffect/VideoEffectChainNode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AddShapeEffect.ForVideoEffectChain;
+﻿using System.Collections.Immutable;
 using Vortice.Direct2D1;
 using Vortice.Direct2D1.Effects;
 using YukkuriMovieMaker.Commons;
